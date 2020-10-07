f119843c9fe1f31806689a1ecd1d0b2d93a1cafb
Prathap <42710476+prathapgowda412@users.noreply.github.com>
2020-10-07 16:07:12 +0530

    kjdf

--- src/App.js
+++ src/App.js
@@ -5,12 +5,7 @@ import './App.css';
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				helo
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
+			<header className="App-header">uniquefit</header>
 		</div>
 	);
 }
