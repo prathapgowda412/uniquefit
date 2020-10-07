f570aaa4cc6d0489a86ea1b7f5e21c236f484cef
Prathap <42710476+prathapgowda412@users.noreply.github.com>
2020-10-07 15:54:42 +0530

    Updates

---
+++ precache-manifest.94821598d1fc8ea20b2ac3f12711fe7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db065ca80c5e0c1b4b73a2dba0354fe",
+    "url": "/uniquefit/index.html"
+  },
+  {
+    "revision": "8dfab2520a69eee208cf",
+    "url": "/uniquefit/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f4b7dc10ff3339f5eaad",
+    "url": "/uniquefit/static/js/2.a854b374.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/uniquefit/static/js/2.a854b374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dfab2520a69eee208cf",
+    "url": "/uniquefit/static/js/main.982e2361.chunk.js"
+  },
+  {
+    "revision": "80ca6c9fc2b5b5d70e00",
+    "url": "/uniquefit/static/js/runtime-main.dcf89e55.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/uniquefit/static/media/logo.5d5d9eef.svg"
+  }
+]);
