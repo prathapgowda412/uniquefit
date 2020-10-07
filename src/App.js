71eb16941a929cd6d5bf064631d44c93ee810524
Prathap <42710476+prathapgowda412@users.noreply.github.com>
2020-10-07 16:25:27 +0530

    nom

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">uniquefit</header>
-		</div>
-	);
+	return <div>helo welome</div>;
 }
 
 export default App;
