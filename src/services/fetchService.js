513b3bee6768b4f03fa75e17c4716914804258f0
chrisstone07 <chrischris_4425@hotmail.com>
2020-11-26 01:22:24 +0530

    added orders and customisations services

--- src/services/fetchService.js
+++ src/services/fetchService.js
@@ -1,33 +1,54 @@
-import httpService from './httpService';
-import config from '../config.json';
-import { getUserData } from './authService';
+import httpService from "./httpService";
+import config from "../config.json";
+import { getUserData } from "./authService";
 
 export function getProducts() {
-	return httpService.get(config.getProducts);
+  return httpService.get(config.getProducts);
 }
 
 export function addToCart(item) {
-	let userId = getUserData();
-	let itemData = {
-		userId,
-		item,
-	};
-	return httpService.post(config.addToCart, itemData);
+  let userId = getUserData();
+  let itemData = {
+    userId,
+    item,
+  };
+  return httpService.post(config.addToCart, itemData);
 }
 
 export function removeFromCart(productid) {
-	let userId = getUserData();
-	let prodData = {
-		userId,
-		productid,
-	};
-	return httpService.post(config.removeFromCart, prodData);
+  let userId = getUserData();
+  let prodData = {
+    userId,
+    productid,
+  };
+  return httpService.post(config.removeFromCart, prodData);
 }
 
 export function getCartItems() {
-	const userId = getUserData();
-	let data = {
-		userId,
-	};
-	return httpService.post(config.getCartItems, data);
+  const userId = getUserData();
+  let data = {
+    userId,
+  };
+  return httpService.post(config.getCartItems, data);
+}
+
+export function getOrders() {
+  const userId = getUserData();
+  let data = {
+    userId,
+  };
+  return httpService.post(config.getOrders, data);
+}
+
+export function addOrder(products) {
+  const userId = getUserData();
+  let data = {
+    userId,
+    products,
+  };
+  return httpService.post(config.addOrder, data);
+}
+
+export function getCustomisations() {
+  return httpService.get(config.getCustomisations);
 }
