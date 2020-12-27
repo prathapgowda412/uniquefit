1a04783555eb55cf941a601b6e8123050eddf9a0
Prathap <42710476+prathapgowda412@users.noreply.github.com>
2020-12-27 12:01:30 +0530

    doing filter and auth

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -26,6 +26,8 @@ import Axios from 'axios';
 import { toast } from 'react-toastify';
 import AddProduct from './AddProduct/addProduct';
 import DeleteProduct from './DeleteProduct/deleteProduct';
+import { Redirect } from 'react-router-dom';
+import { Person, PersonPinCircleRounded } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
 	header: {
@@ -53,6 +55,13 @@ const useStyles = makeStyles((theme) => ({
 	rightpanel: {
 		borderRadius: '10px',
 		width: '100%',
+	},
+	avatrbox: {
+		display: 'flex',
+		flexDirection: 'row',
+		width: '160px',
+		alignItems: 'center',
+		justifyContent: 'space-around',
 	},
 }));
 
@@ -92,71 +101,90 @@ function Dashboard() {
 	const handleTabChange = (event, newValue) => {
 		settabValue(newValue);
 	};
+	const [isAdminLogged, setIsAdminLogged] = React.useState(false);
+	// useEffect(() => {
+	// 	const adminToken = localStorage.getItem('adminToken');
+	// 	if (adminToken) {
+	// 		setIsAdminLogged(true);
+	// 	} else {
+	// 		setIsAdminLogged(false);
+	// 	}
+	// });
 
 	const classes = useStyles();
-	return (
-		<Grid item container xs={12}>
-			<Grid item container xs={12} className={classes.header}>
-				<Grid xs={2}>
-					{/* <Typography variant="h5">Admin Dashboard</Typography> */}
-					<img height="60px" src={require('../../logos/Uniquefit logo.svg')} />
-				</Grid>
+	const handleLogOut = async () => {
+		localStorage.removeItem('adminToken');
+		window.location.reload();
+	};
+	if (localStorage.getItem('adminToken')) {
+		return (
+			<Grid item container xs={12}>
+				<Grid item container xs={12} className={classes.header}>
+					<Grid xs={2}>
+						{/* <Typography variant="h5">Admin Dashboard</Typography> */}
+						<img height="60px" src={require('../../logos/Uniquefit logo.svg')} />
+					</Grid>
 
-				<Grid xs={8}></Grid>
+					<Grid xs={8}></Grid>
 
-				<Grid xs={2}>
-					<Container className={classes.avatrbox}>
-						{/* <Avatar /> */}
-						<img height="25px" src={require('./user.svg')} />
-						<Typography variant="h6"> Admin</Typography>
-					</Container>
-				</Grid>
-			</Grid>
-			<Hidden smDown>
-				<Grid item className={classes.bottom} container xs={12}>
-					<Grid item container xs={2} className={classes.leftbox}>
-						<Container className={classes.deskmenu}>
-							<Tabs
-								indicatorColor="none"
-								orientation="vertical"
-								value={tabvalue}
-								onChange={handleTabChange}
-								aria-label="Vertical tabs example"
-								className={classes.lefttabs}>
-								<Tab className={classes.menulinks} label="Overview" {...a11yProps(0)}></Tab>
-								<Tab className={classes.menulinks} label=" Products" {...a11yProps(1)} />
-								<Tab className={classes.menulinks} label="Orders" {...a11yProps(2)} />
-								{/* <Tab className={classes.menulinks} label="Category" {...a11yProps(3)} /> */}
-							</Tabs>
+					<Grid xs={2}>
+						<Container className={classes.avatrbox}>
+							{/* <Avatar /> */}
+							{/* <img height="25px" src={require('./user.svg')} /> */}
+							<Person fontSize="large" />
+							<Typography style={{ fontSize: '20px', color: '#030303' }}> Admin</Typography>
 						</Container>
-					</Grid>
-					<Grid item container xs={10} className={classes.rightbox}>
-						<Box className={classes.rightpanel}>
-							<TabPanel className={classes.righttabpanel} value={tabvalue} index={0}>
-								{' '}
-								under development
-							</TabPanel>
-
-							<TabPanel className={classes.righttabpanel} value={tabvalue} index={1}>
-								<Grid container style={{ width: '100%' }}>
-									<AddProduct />
-									<DeleteProduct />
-								</Grid>
-							</TabPanel>
-
-							<TabPanel value={tabvalue} index={2}>
-								3
-							</TabPanel>
-							{/* <TabPanel value={tabvalue} index={3}>
-								rtr
-							</TabPanel> */}
-						</Box>
+						<Button onClick={handleLogOut}>Logout</Button>
 					</Grid>
 				</Grid>
-			</Hidden>
-			<Hidden mdUp>helo down</Hidden>
-		</Grid>
-	);
+				<Hidden smDown>
+					<Grid item className={classes.bottom} container xs={12}>
+						<Grid item container xs={2} className={classes.leftbox}>
+							<Container className={classes.deskmenu}>
+								<Tabs
+									indicatorColor="none"
+									orientation="vertical"
+									value={tabvalue}
+									onChange={handleTabChange}
+									aria-label="Vertical tabs example"
+									className={classes.lefttabs}>
+									<Tab className={classes.menulinks} label="Overview" {...a11yProps(0)}></Tab>
+									<Tab className={classes.menulinks} label=" Products" {...a11yProps(1)} />
+									<Tab className={classes.menulinks} label="Orders" {...a11yProps(2)} />
+									{/* <Tab className={classes.menulinks} label="Category" {...a11yProps(3)} /> */}
+								</Tabs>
+							</Container>
+						</Grid>
+						<Grid item container xs={10} className={classes.rightbox}>
+							<Box className={classes.rightpanel}>
+								<TabPanel className={classes.righttabpanel} value={tabvalue} index={0}>
+									{' '}
+									under development
+								</TabPanel>
+
+								<TabPanel className={classes.righttabpanel} value={tabvalue} index={1}>
+									<Grid container style={{ width: '100%' }}>
+										<AddProduct />
+										<DeleteProduct />
+									</Grid>
+								</TabPanel>
+
+								<TabPanel value={tabvalue} index={2}>
+									3
+								</TabPanel>
+								{/* <TabPanel value={tabvalue} index={3}>
+									rtr
+								</TabPanel> */}
+							</Box>
+						</Grid>
+					</Grid>
+				</Hidden>
+				<Hidden mdUp>helo down</Hidden>
+			</Grid>
+		);
+	} else {
+		return <Redirect to="/adminLogin" />;
+	}
 }
 
 export default Dashboard;
